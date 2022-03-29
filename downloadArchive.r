224f95aed7f1503bac46ab3d1a9304ebaa061aa3
Lanka <86649084+NIKHIL-505@users.noreply.github.com>
2022-03-29 20:03:49 +0530

    Add files via upload

---
+++ downloadArchive.r
@@ -0,0 +1,12 @@
+archiveFile <- "NEI_data.zip"
+
+if(!file.exists(archiveFile)) {
+  
+  archiveURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  
+  download.file(url=archiveURL,destfile=archiveFile)
+}
+
+if(!(file.exists("summarySCC_PM25.rds") && file.exists("Source_Classification_Code.rds"))) { 
+  unzip(archiveFile) 
+}
